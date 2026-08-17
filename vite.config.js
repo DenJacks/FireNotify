59569cec826e9c99952c49f38dde730efa445db4
DenJacks <soenacned@gmail.com>
2026-08-17 20:43:21 +0800

    Pulling_code_in_my_pc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  plugins: [vue(), tailwindcss()],
 })
